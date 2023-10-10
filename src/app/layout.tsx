6123bd2d3f05c97e08a5cdee434a298c93dc31b4
Andrey Kurtenkov <kurtenkov.as@gmail.com>
2023-10-10 23:48:26 +0300

    feat: eslint rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import './globals.scss';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import '@/styles/globals.scss';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -12,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
